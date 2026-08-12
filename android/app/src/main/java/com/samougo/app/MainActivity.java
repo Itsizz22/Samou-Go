a0deb4663c6f0f1eff29acba5b203333efe2b0bc
Itsizz22 <azzdaghmeen@gmail.com>
2026-08-12 09:22:10 +0300

    MVP:2.1

---
+++ android/app/src/main/java/com/samougo/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.samougo.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.samougo.app',
+  appName: 'samou-go',
+  webDir: 'dist'
+};
+
+export default config;
